9a698224d56128c28a7117e4d8df953a6c81db96
krow69 <krowxen2214@gmail.com>
2023-11-20 00:27:29 +0000

    workShop3 Refactor the script you created above to be generic

--- workShop3/convert.php
+++ workShop3/convert.php
@@ -1,45 +1,51 @@
 <?php
-// Input CSV file
-
+    function convertToXML($input_file) {
+        // Input CSV file
 // Output XML file
-$outputFilename = 'output.xml';
+        $outputFilename = 'output.xml';
 
-// Open CSV file for reading
-$inputFile = fopen('D:\fullStackEnvironment\laragon\www\Web-Development-Course-WorkShop\workShop3\quotes.csv', 'r');
+        // Open CSV file for reading
+        $input_file = fopen($input_file, 'r');
 
-if ($inputFile !== false) {
-    // Read the headers (first row) to use as XML element names
-    $headers = fgetcsv($inputFile, 1000, '|');
-    
-    // Create a new XML document
-    $doc = new DOMDocument();
-    $doc->formatOutput = true;
+        if ($input_file !== false) {
+            // Read the headers (first row) to use as XML element names
+            $headers = fgetcsv($input_file, 1000, '|');
 
-    // Create a root element for the XML
-    $root = $doc->createElement('data');
-    $root = $doc->appendChild($root);
+            // Create a new XML document
+            $doc = new DOMDocument();
+            $doc->formatOutput = true;
 
-    // Loop through each row in the CSV
-    while (($row = fgetcsv($inputFile, 1000, '|')) !== false) {
-        $rowElement = $doc->createElement('record');
-        
-        foreach ($headers as $index => $header) {
-            $field = $doc->createElement($header);
-            $field->appendChild($doc->createTextNode($row[$index]));
-            $rowElement->appendChild($field);
+            // Create a root element for the XML
+            $root = $doc->createElement('data');
+            $root = $doc->appendChild($root);
+
+            // Loop through each row in the CSV
+            $recordId = 1;
+            while (($row = fgetcsv($input_file, 1000, '|')) !== false) {
+                $rowElement = $doc->createElement('record');
+                $rowElement->setAttribute('id', $recordId);
+                $rowElement->setAttribute('category', $row[count($row) - 1]);
+                foreach ($headers as $index => $header) {
+                    $field = $doc->createElement($header);
+                    $field->appendChild($doc->createTextNode($row[$index]));
+                    $rowElement->appendChild($field);
+                }
+
+                $root->appendChild($rowElement);
+                $recordId++;
+            }
+
+            // Save the XML to the output file
+            $doc->save($outputFilename);
+
+            // Close the CSV file
+            fclose($input_file);
+
+            echo "Conversion completed. Output file: $outputFilename";
+        } else {
+            echo "Failed to open the input CSV file.";
         }
-        
-        $root->appendChild($rowElement);
     }
 
-    // Save the XML to the output file
-    $doc->save($outputFilename);
-
-    // Close the CSV file
-    fclose($inputFile);
-
-    echo "Conversion completed. Output file: $outputFilename";
-} else {
-    echo "Failed to open the input CSV file.";
-}
+    convertToXML('D:\fullStackEnvironment\laragon\www\Web-Development-Course-WorkShop\workShop3\quotes.csv');
 ?>
